79bf6adbd3c3093cdf192df3a0d7bd220a57888e
Roberto Tatasciore <tatasciorecont@gmail.com>
2021-08-04 19:51:45 +0200

    Add test reducer and redux-flipper

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,28 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore, createSlice } from '@reduxjs/toolkit';
+
+const l18nSlice = createSlice({
+  name: 'l18n',
+  initialState: {
+    locale: 'en',
+  },
+  reducers: {},
+});
 
 export const store = configureStore({
-  reducer: combineReducers({}),
+  reducer: combineReducers({
+    l18n: l18nSlice.reducer,
+  }),
+  middleware: defaultMiddleware => {
+    const middlewares = defaultMiddleware();
+
+    if (__DEV__) {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const createDebugger = require('redux-flipper').default;
+      middlewares.push(createDebugger());
+    }
+
+    return middlewares;
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
