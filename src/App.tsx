a208bf635e8ad2b4656f4c5c8226d7e440bf682d
Roberto Tatasciore <tatasciorecont@gmail.com>
2021-08-04 18:56:57 +0200

    Add redux and react-redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppNavigator } from '@utils/routes';
 import { I18nProvider } from '@modules/I18nProvider/I18nProvider';
+import { store } from 'store';
 
 export const App = () => (
-  <I18nProvider>
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
-  </I18nProvider>
+  <Provider store={store}>
+    <I18nProvider>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </I18nProvider>
+  </Provider>
 );
